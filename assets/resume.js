5ae1ebdd60859794903e20f4997feea85f07005c
xhoemawn <khadkasuman20@gmail.com>
2019-05-04 22:55:07 +0545

    asd

---
+++ assets/resume.js
@@ -0,0 +1,25 @@
+let resume = {
+    "name": "Suman Khadka",
+    "qualification" : "Bachelors Degree In Information Technology",
+    "jobHistory":[
+        {
+            "companyName": "All Star IT Solution",
+            "position": "Java Developer",
+            "from": "2017",
+            "to": "2018",
+            "techUsed": "Spring Boot, JSF Primefaces"
+        },
+        {
+            "companyName": "All Star IT Solution",
+            "position": "Android Developer",
+            "from": "2018",
+            "to": "2019",
+            "techUsed": "Android SDK, Retrofit"
+        }
+        
+    ],
+    "jobStatus": "Unemployed",
+    "email": "khadkasuman20@gmail.com",
+    "number": "+9779860472874",
+    "skills": "can sit and do nothing all day"
+}
